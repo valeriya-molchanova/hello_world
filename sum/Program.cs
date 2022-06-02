f99fee7b87b114dece4ec502e0478b3ef64c07a3
Valeriya <molchanova9080@gmail.com>
2022-06-02 21:25:45 +0800

    ram random

--- sum/Program.cs
+++ sum/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 85;
-int numberB = 47;
+﻿int numberA = new Random().Next(1, 100); // 1 2 3 4 ..... 99
+int numberB = new Random().Next(1, 100); // 1 2 3 4 ..... 99
 int result = numberA + numberB;
 Console.WriteLine(result);
