d13597d533beba948653a7c0f9a725690470f9fa
Valeriya <molchanova9080@gmail.com>
2022-06-02 22:17:10 +0800

    like username

---
+++ condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Лера")
+{
+    Console.WriteLine("Ура, это же Лера!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
